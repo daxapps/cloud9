3d24969f4df2506a7ad133a2f12cad13a8f5498d
daxapps <daxapps777@gmail.com>
2017-04-09 20:54:25 -0500

    RESTful Blog App: NEW and CREATE
    - Add NEW route
    - Add NEW template
    - Add CREATE route
    - Add CREATE template

--- workspace/RESTful/RESTfulBlogApp/app.js
+++ workspace/RESTful/RESTfulBlogApp/app.js
@@ -22,12 +22,32 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req,res){
     res.redirect("/blogs");
 });
+
+// INDEX route
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
             console.log(err);
         } else {
             res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// NEW route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+// CREATE route
+app.post("/blogs", function(req, res){
+    //crete blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            //then, redirect to the index
+            res.redirect("/blogs");
         }
     });
 });
